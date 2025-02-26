3f81dacc4365a8e69477391e05f53d841654f6c2
FlyinqR <raymond.wang200921@gmail.com>
2025-02-26 17:15:48 -0500

    ActivatedPlate Var

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
+    public int ActivatedPressure;
+
     // Start is called before the first frame update
     void Start()
     {
