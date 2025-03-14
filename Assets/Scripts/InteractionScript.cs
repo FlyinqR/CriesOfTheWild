2b66d305c063d56b697aeb4462ad69fa79f31a49
FlyinqR <raymond.wang200921@gmail.com>
2025-03-14 18:28:41 -0400

    PuzzleSelection
    Certain puzzle fit with certain puzzle

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -9,6 +9,8 @@ public class InteractionScript : MonoBehaviour
     private int activatedPressurePlates = 0; // Track activated plates
     public int totalItems = 5; // Set this based on the number of items
     private int itemsCollected = 0; // Track collected items
+    private Material boxColor;
+    private Material PressurePlateColor;
 
     private void OnTriggerStay(Collider collision)
     {
@@ -18,17 +20,30 @@ public class InteractionScript : MonoBehaviour
             itemsCollected++; // Increase collected item count
             Debug.Log("Item Collected: " + itemsCollected);
             Destroy(collision.gameObject);
+            boxColor = collision.gameObject.GetComponent<MeshRenderer>().materials[0];
+            Debug.Log(boxColor);
+
         }
 
         if (collision.gameObject.tag == "PressurePlate" && HoldingItem && Input.GetMouseButton(1))
         {
-            Debug.Log("Item Dropped");
-            HoldingItem = false;
-            activatedPressurePlates++; // Increase activated plate count
-            Debug.Log("Pressure Plate Activated: " + activatedPressurePlates);
-            collision.gameObject.SetActive(false);
+            PressurePlateColor = collision.gameObject.GetComponent<MeshRenderer>().materials[0];
+            Debug.Log(PressurePlateColor);
+            if (boxColor == PressurePlateColor)
+            {
+                Debug.Log("Item Dropped");
+                HoldingItem = false;
+                activatedPressurePlates++; // Increase activated plate count
+                Debug.Log("Pressure Plate Activated: " + activatedPressurePlates);
+                collision.gameObject.SetActive(false);
 
-            CheckLevelCompletion(); // Check if all plates are activated
+                CheckLevelCompletion(); // Check if all plates are activated
+            }
+            else 
+            {
+                Debug.Log("Wrong Piece");
+            }
+           
         }
     }
 
@@ -53,4 +68,6 @@ public class InteractionScript : MonoBehaviour
             Debug.Log("No more levels available!");
         }
     }
+
+    
 }
