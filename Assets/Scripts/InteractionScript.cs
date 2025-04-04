ca5ac95a2d159b5bf3ffe6e44f4078c65a75dacb
fawazeen <fawazameen.sia@gmail.com>
2025-04-04 18:48:55 -0400

    LoseScreen And Timer finished

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Import Scene Management
+using UnityEngine.SceneManagement;
+using TMPro;   // Import Scene Management
+using UnityEngine.UI;
 
 public class InteractionScript : MonoBehaviour
 {
@@ -11,7 +13,68 @@ public class InteractionScript : MonoBehaviour
     private int itemsCollected = 0; // Track collected items
     public Material boxColor;
     public Material PressurePlateColor;
+    [Header("Timer Settings")] // ADD >>>
+    [SerializeField] private float timeRemaining = 300f; // 5 minutes
+    private bool timerRunning = true;
+    [Header("UI Reference")] // ADD >>>
+    public TextMeshProUGUI timerText;
+    [Header("Lose Screen UI")] // ADD >>>
+    public GameObject loseScreenUI;
+    public Button restartButton;
+    public Button quitButton;
 
+    void Update()
+    {
+        if (timerRunning) // ADD >>>
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimerUI(timeRemaining);
+
+            if (timeRemaining <= 0)
+            {
+                timerRunning = false;
+                Debug.Log("Time's up! Level failed.");
+                HandleLevelFailure(); // Restart level or custom fail logic
+            }
+        }
+    }
+
+
+    private void HandleLevelFailure()
+    {
+        timerRunning = false;
+        loseScreenUI.SetActive(true); // Show lose screen
+        Time.timeScale = 0f; // Pause the game
+
+        restartButton.onClick.RemoveAllListeners(); // Just to be safe
+        restartButton.onClick.AddListener(RestartLevel);
+
+        quitButton.onClick.RemoveAllListeners();
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+
+
+    private void RestartLevel()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void QuitGame()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+        Debug.Log("Game Quit");
+    }
+
+    void UpdateTimerUI(float time) // ADD >>>
+    {
+        time = Mathf.Max(0, time);
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
     private void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.tag == "Interactable" && !HoldingItem && Input.GetMouseButton(1))
@@ -19,7 +82,7 @@ public class InteractionScript : MonoBehaviour
             HoldingItem = true;
             itemsCollected++; // Increase collected item count
             Debug.Log("Item Collected: " + itemsCollected);
-            MeshRenderer boxColor = collision.gameObject.GetComponent<MeshRenderer>();
+            boxColor = collision.gameObject.GetComponent<MeshRenderer>().materials[0];
             Debug.Log(boxColor);
             Destroy(collision.gameObject);
 
@@ -27,9 +90,9 @@ public class InteractionScript : MonoBehaviour
 
         if (collision.gameObject.tag == "PressurePlate" && HoldingItem && Input.GetMouseButton(1))
         {
-            MeshRenderer PressurePlateColor = collision.gameObject.GetComponent<MeshRenderer>();
+            PressurePlateColor = collision.gameObject.GetComponent<MeshRenderer>().materials[0];
             Debug.Log(PressurePlateColor);
-            if (PressurePlateColor.sharedMaterial == boxColor)
+            if (PressurePlateColor.name == boxColor.name)
             {
                 Debug.Log("Item Dropped");
                 
