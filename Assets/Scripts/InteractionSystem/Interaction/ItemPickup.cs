8d355237ec758268dd06692cb867b2a0763232a5
FlyinqR <raymond.wang200921@gmail.com>
2025-05-28 18:43:04 -0400

    Finished Complete Interaction System
    Added UI as well

---
+++ Assets/Scripts/InteractionSystem/Interaction/ItemPickup.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour, IInteractable
+{
+    private bool hasInteracted = false;
+    public InteractionScript interactionScript;
+
+
+    public bool CanInteract()
+    {
+        if (hasInteracted) 
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool Interact(PlayerInteraction interactor)
+    {
+        interactionScript.HoldingItem = true;
+        interactionScript.itemsCollected++; // Increase collected item count
+        Debug.Log("Item Collected: " + interactionScript.itemsCollected);
+        //interactionScript.boxColor = collision.gameObject.GetComponent<MeshRenderer>().materials[0];
+        Debug.Log(interactionScript.boxColor);
+        Destroy(gameObject);
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionScript = GameObject.Find("InteractionRadius").GetComponent<InteractionScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
